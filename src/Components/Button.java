377d0863b9ab46f0e3841747a62bdbc0907ae033
Raahim Fareed <raahim1965@hotmail.com>
2023-06-05 05:34:02 +0500

    removed height and width

--- src/Components/Button.java
+++ src/Components/Button.java
@@ -1,15 +1,10 @@
 package Components;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Button extends JButton {
     public Button(String title) {
         super(title);
-        this.setSize(256, 64);
-    }
-
-    public Button(String title, int width, int height) {
-        super(title);
-        this.setSize(width, height);
     }
 }
