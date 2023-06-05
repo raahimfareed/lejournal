327fb06f8ab9ba26df3728aff97385d21e6e064b
Raahim Fareed <raahim1965@hotmail.com>
2023-06-05 06:07:17 +0500

    added a way to add on action event for button

--- src/Components/Button.java
+++ src/Components/Button.java
@@ -2,9 +2,23 @@ package Components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Button extends JButton {
+    private ActionListener actionListener;
     public Button(String title) {
         super(title);
+    }
+
+    public void setOnAction(ActionListener listener) {
+        this.actionListener = listener;
+        addActionListener(listener);
+    }
+
+    public void removeOnAction() {
+        if (this.actionListener == null) return;
+
+        removeActionListener(this.actionListener);
+        this.actionListener = null;
     }
 }
