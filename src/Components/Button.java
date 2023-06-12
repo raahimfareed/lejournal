a23d294f273f23cb8bfd2e4409f15d74b691a38c
raahimfareed <raahimfareed@proton.me>
2023-06-12 15:29:13 +0500

    refactored: removed unnecessary property

--- src/Components/Button.java
+++ src/Components/Button.java
@@ -5,14 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Button extends JButton {
-    private ActionListener actionListener;
     public Button(String title) {
         super(title);
         this.setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
     public void setOnAction(ActionListener listener) {
-        this.actionListener = listener;
         addActionListener(listener);
     }
 
